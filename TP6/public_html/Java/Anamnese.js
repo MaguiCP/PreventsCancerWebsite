97a1c88938e4598dc6c03ed4a180d81fc6792fca
Margarida <maguicpereira@gmail.com>
2024-08-07 15:55:59 +0100

    Initial commit

---
+++ TP6/public_html/Java/Anamnese.js
@@ -0,0 +1,45 @@
+/* 
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/ClientSide/javascript.js to edit this template
+ */
+
+      function exibirPerguntas1() {
+                var perguntas = document.getElementById("perguntas-adicionais1");
+                if (perguntas.style.display === "none") {
+                    perguntas.style.display = "block";
+                } else {
+                    perguntas.style.display = "none";
+                }
+            }
+            function esconderPerguntas1() {
+                var perguntas = document.getElementById("perguntas-adicionais1");
+                if (perguntas.style.display === "block") {
+                    perguntas.style.display = "none";
+                } else {
+                    perguntas.style.display = "block";
+                }
+            }
+            function exibirPerguntas2() {
+                var perguntas = document.getElementById("perguntas-adicionais2");
+                if (perguntas.style.display === "none") {
+                    perguntas.style.display = "block";
+                } else {
+                    perguntas.style.display = "none";
+                }
+            }
+            function exibirPerguntas3() {
+                var perguntas = document.getElementById("perguntas-adicionais3");
+                if (perguntas.style.display === "none") {
+                    perguntas.style.display = "block";
+                } else {
+                    perguntas.style.display = "none";
+                }
+            }
+            function exibirPerguntas4() {
+                var perguntas = document.getElementById("perguntas-adicionais4");
+                if (perguntas.style.display === "none") {
+                    perguntas.style.display = "block";
+                } else {
+                    perguntas.style.display = "none";
+                }
+            }
